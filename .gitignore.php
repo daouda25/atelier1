41b1f82287e45e38462e04bf78c5767348262f21
fayedethie <fayedethie20@gmail.com>
2021-05-04 10:50:18 +0000

    ignore

---
+++ .gitignore.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head
+<meta charset="uft-8">
+<tiltle> location de voiture au senegal</tiltle>
+</head>
+<body>
+<h1> mon fichier gitignore</h1>
+<p>pour ignorer plusieurs element a la ,une fois que vous avez tape le nom d un fichierou d un dossier sur une ligne</p>
+
+</body>
+</html>
