9568475b863c973384073a8aa3f224ad4770bfd6
fayedethie <fayedethie20@gmail.com>
2021-05-04 07:20:52 +0000

    commentaire

---
+++ index2.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head
+<meta charset="uft-8">
+<tiltle> location de voiture au senegal</tiltle>
+</head>
+<body>
+<h1> voila mon premier mon projet en codage </h1>
+<p> apprendre a coder<p>
+
+</body>
+</html>
